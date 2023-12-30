6e6ebd88adbca68acd1df051a6201c2dd772ad58
buddhathemanoj <prabhakarmanoj743@gmail.com>
2023-12-30 19:56:59 +0530

    facecam

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,32 +1,29 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import QrScanner from "react-qr-scanner";
-import "./App.css";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { MdOutlineCameraswitch } from "react-icons/md";
 import { retrieveDataById } from "./firebaseuttils";
+import QrReader from "react-qr-reader";
+import "./App.css";
+
 const Scanner = () => {
   const navigate = useNavigate();
   const [code, setCode] = useState(null);
-  const [showDialog, setDiaglog] = useState(false);
+  const [showDialog, setDialog] = useState(false);
   const [processing, setProcessing] = useState(false);
   const [precScan, setPrecScan] = useState("");
   const [selected, setSelected] = useState("rear");
   const [errorMessage, setErrorMessage] = useState(null);
 
-
   const handleScan = async (scanData) => {
-
     if (scanData && scanData !== "" && !showDialog && !processing) {
-      console.log(`loaded >>>`, scanData.text);
+      console.log(`loaded >>>`, scanData);
       setSelected("");
-      setPrecScan(scanData.text);
+      setPrecScan(scanData);
 
       try {
-        const data = await retrieveDataById(scanData.text);
+        const data = await retrieveDataById(scanData);
         if (data && data !== null) {
-          navigate(`/details/${scanData.text}`, { state: { data } });
-
+          navigate(`/details/${scanData}`, { state: { data } });
         }
         console.log("Data retrieved from Firestore:", data);
       } catch (error) {
@@ -35,51 +32,36 @@ const Scanner = () => {
     }
   };
 
-  useEffect(() => {
-    const isMobileDevice =
-      navigator.userAgent &&
-      /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    if (isMobileDevice) {
-      setSelected("rear");
-    } else {
-      setSelected("front");
-    }
-  }, []);
-
-
   const handleError = (err) => {
     console.error(err);
   };
 
   const toggleCamera = () => {
-    setSelected("rear");
+    setSelected(selected === "rear" ? "front" : "rear");
   };
 
   return (
     <div className="App">
-      <h2 className="custom-logo-head">ARLUMIGU THANDAYUTHAPANI KOVIL</h2>
+      <h2 className="custom-logo-head">
+        ARLUMIGU THANDAYUTHAPANI KOVIL
+      </h2>
       <h2 className="custom-logo-head">அருள்மிகு தண்டாயுதபாணி கோவில்</h2>
-      {/* <h2>Last Scan:{precScan} {selected}</h2> */}
-
-
 
       <div className="camerabox">
         {!showDialog && !processing && (
-          <QrScanner
-            facingMode={selected} // Set facingMode explicitly to 'environment' for the back camera
+          <QrReader
+          style={{ width: "300px" }}
+
+            facingMode={selected}
             delay={500}
-            key="environment"
             onError={handleError}
             onScan={handleScan}
-            style={{ width: "100%" }}
-            legacyMode={false}
           />
         )}
-        {/* <button onClick={toggleCamera} className="camera-toggle-button">
-          {selected === "rear" ? <MdOutlineCameraswitch /> : <MdOutlineCameraswitch />}
-        </button> */}
+        <button onClick={toggleCamera} className="camera-toggle-button">
+          <MdOutlineCameraswitch />
+        </button>
       </div>
-
     </div>
   );
 };
